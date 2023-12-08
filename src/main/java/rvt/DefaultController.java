fba7ac10c3b0a96451fdd80e8b240825c68acf8b
22DP1DVasi <131352712+22DP1DVasi@users.noreply.github.com>
2023-12-08 12:10:18 +0000

    html about file

--- src/main/java/rvt/DefaultController.java
+++ src/main/java/rvt/DefaultController.java
@@ -8,8 +8,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class DefaultController {
     
-   @GetMapping(value = "/")
+   @GetMapping(value = "/")     // lapas adrese (URL)
     String index(@RequestParam(name="name", required=false, defaultValue="null") String name, Model model) {
         return "index";
+    }
+
+    @GetMapping(value = "/about")
+    String aboutMe(Model model) {
+        String name = "Danila";
+        String surname = "Vasiljevs";
+        int age = 17;
+        String group = "DP2-1";
+        model.addAttribute("name", name);
+        model.addAttribute("surname", surname);
+        model.addAttribute("age", age);
+        model.addAttribute("group", group);
+
+        return "about";
     }
 }
